386b7bf3538a341930aa7950f248102f0e2beb41
Krushnabirhare <96606868+Krushnabirhare@users.noreply.github.com>
2022-01-10 22:52:49 +0530

    Add files via upload

---
+++ ques_1.py
@@ -0,0 +1,7 @@
+# Question 1
+a=input("Enter first number :")
+b=input("Enter second number :")
+c=input("Enter third number :")
+d=(int(a)+int(b)+int(c))/3
+print("The average of three numbers is :")
+print(float(d))
---
+++ ques_3.py
@@ -0,0 +1,8 @@
+# Question 3
+SID=input("Enter your SID :")
+Name=input("Enter your name :")
+Gender=input("Enter your Gender :")
+Course_name=input("Enter your course name :")
+CGPA=float(input("Enter your CGPA :"))
+STUDENT=[SID,Name,Gender,Course_name,CGPA]
+print(STUDENT)
