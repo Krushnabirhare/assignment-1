6822702af30c1c4e89a1a067b4d1553e89d47104
Krushnabirhare <96606868+Krushnabirhare@users.noreply.github.com>
2022-01-13 21:50:46 +0530

    Delete ques_5.py

--- ques_5.py
+++
@@ -1,7 +0,0 @@
-# Question 5
-colour=['Red','Green','White','Black','Pink','Yellow']
-colour.remove(colour[3])
-print(" (A) : ",colour)
-colour=['Red','Green','White','Black','Pink','Yellow']
-colour[3:5]=['Purple']
-print("(B) :",colour)
