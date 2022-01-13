4072e782811189b5f740b88162b0356d5037e68e
Krushnabirhare <96606868+Krushnabirhare@users.noreply.github.com>
2022-01-13 13:32:05 +0530

    Delete ques_3.py

--- ques_3.py
+++
@@ -1,8 +0,0 @@
-# Question 3
-SID=input("Enter your SID :")
-Name=input("Enter your name :")
-Gender=input("Enter your Gender :")
-Course_name=input("Enter your course name :")
-CGPA=float(input("Enter your CGPA :"))
-STUDENT=[SID,Name,Gender,Course_name,CGPA]
-print(STUDENT)
